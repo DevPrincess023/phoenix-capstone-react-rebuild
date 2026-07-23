6c3d80b0edfe252b5a73810dd879fe4c260de849
Ezeobi Mercy <chimfurumnanyamercy@gmail.com>
2026-07-23 03:33:19 -0700

    Initial Vite React setup with component structure

---
+++ phoenix-capstone-react/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Video from './components/Video';
+import PlanetGrid from './components/PlanetGrid';
+import Table from './components/Table';
+import Form from './components/Form';
+import Footer from './components/Footer';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Video />
+      <PlanetGrid />
+      <Table />
+      <Form />
+      <Footer />
+    </>
+  );
+}
