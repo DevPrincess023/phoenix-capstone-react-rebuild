53f4ca47d286ec23ade538cc0237e9dd18caf303
Ezeobi Mercy <chimfurumnanyamercy@gmail.com>
2026-07-24 01:09:29 -0700

    Add Header component with basic styling

--- phoenix-capstone-react/src/components/Header.jsx
+++ phoenix-capstone-react/src/components/Header.jsx
@@ -1,3 +1,13 @@
+import './Header.css';
+
 export default function Header() {
-    return <header>Header</header>;
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <a href="#" className="navbar-logo">
+                    Phoenix
+                </a>
+            </div>
+        </nav>
+    );
 }
